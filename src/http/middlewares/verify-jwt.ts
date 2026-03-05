5a9e87b47a58914aff668f5ed2300f8808162060
Seu Nome <thofurtado@gmail.com>
2026-03-05 11:48:36 -0300

    Update verify-jwt.ts

--- src/http/middlewares/verify-jwt.ts
+++ src/http/middlewares/verify-jwt.ts
@@ -1,6 +1,13 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function verifyJwt(request: FastifyRequest, reply: FastifyReply) {
+    const apiKey = request.headers['x-api-key']
+    const validKey = process.env.API_KEY_PONTO || 'metrics_secret_key_2026'
+
+    if (apiKey === validKey) {
+        return
+    }
+
     try {
         await request.jwtVerify()
     } catch (err) {
